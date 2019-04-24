d9cc1e2208d646d71628cf82a9a3b73bb7a0097c
Richard Leggett <richard.leggett@earlham.ac.uk>
2019-04-24 11:40:39 +0100

    Update R script

--- scripts/plot_amr_heatmaps.R
+++ scripts/plot_amr_heatmaps.R
@@ -8,6 +8,10 @@ library(params)
 #amrdata <- read.delim("/Users/leggettr/Desktop/BAMBI/P8(200bp,80pc)_group(70pc).txt", sep="\t",head=TRUE, check.names=FALSE)
 #highestv <- max(amrdata[][2:4], na.rm=TRUE)
 
+#pdf("/Users/leggettr/Desktop/BAMBI/P8(100bp,70pc)_group(70pc).pdf", width=3.1, height=11.3);
+#amrdata <- read.delim("/Users/leggettr/Desktop/BAMBI/P8_(100bp70pc)_group(70pc)LH.txt", sep="\t",head=TRUE, check.names=FALSE)
+#highestv <- max(amrdata[][2:4], na.rm=TRUE)
+
 #pdf("/Users/leggettr/Desktop/BAMBI/MockWithSpike(200bp,80pc)_group(70pc).pdf", width=4, height=8);
 #amrdata <- read.delim("/Users/leggettr/Desktop/BAMBI/MockWithSpike(200bp,80pc)_group(70pc).txt", sep="\t",head=TRUE)
 
@@ -23,11 +27,11 @@ library(params)
 #pdf("/Users/leggettr/Desktop/BAMBI/BAMBI_EvenMock_nbc_11032019_insilico_99999(200bp,80pc)_group(70pc).pdf", width=4.5, height=8);
 #amrdata <- read.delim("/Users/leggettr/Desktop/BAMBI/MockInsilico(200bp,80pc)_group(70pc).txt", sep="\t",head=TRUE)
 
-#pdf("/Users/leggettr/Desktop/BAMBI/BAMBI_EvenMock_nbc_11032019_insilico_88888(200bp,80pc)_group(70pc).pdf", width=3.6, height=7);
+#pdf("/Users/leggettr/Desktop/BAMBI/BAMBI_EvenMock_nbc_11032019_insilico_88888(200bp,80pc)_group(70pc).pdf", width=3.6, height=6.5);
 #amrdata <- read.delim("/Users/leggettr/Desktop/BAMBI/MockInsilico8(200bp,80pc)_group(70pc).txt", sep="\t",head=TRUE, check.names=FALSE)
 
-#pdf("/Users/leggettr/Desktop/BAMBI/P103M_Both(200bp,80pc)_group(70pc).pdf", width=4, height=10);
-#amrdata <- read.delim("/Users/leggettr/Desktop/BAMBI/P103M_Both_Spike(200bp,80pc)_group(70pc).txt", sep="\t",head=TRUE, check.names=FALSE)
+pdf("/Users/leggettr/Desktop/BAMBI/P103M_Both(200bp,80pc)_group(70pc).pdf", width=4, height=9.6);
+amrdata <- read.delim("/Users/leggettr/Desktop/BAMBI/P103M_Both_Spike(200bp,80pc)_group(70pc).txt", sep="\t",head=TRUE, check.names=FALSE)
 #highestv <- max(amrdata[][2:5], na.rm=TRUE)
 
 #pdf("/Users/leggettr/Desktop/BAMBI/P103M_40pc(200bp,80pc)_group(70pc).pdf", width=4, height=8);
@@ -42,12 +46,12 @@ library(params)
 #amrdata <- read.delim("/Users/leggettr/Desktop/BAMBI/test_P10_all_(200bp,80pc)_group(70pc).txt", sep="\t",head=TRUE, check.names=FALSE)
 
 # #e41a1c
-#pdf("/Users/leggettr/Desktop/BAMBI/P10_group2_(200bp,80pc)_group(70pc).pdf", width=4, height=11.5);
+#pdf("/Users/leggettr/Desktop/BAMBI/P10_group2_(200bp,80pc)_group(70pc).pdf", width=4, height=8.7);
 #amrdata <- read.delim("/Users/leggettr/Desktop/BAMBI/P10_group2_(200bp,80pc)_group(70pc).txt", sep="\t",head=TRUE, check.names=FALSE)
 
 # #4daf4a
-pdf("/Users/leggettr/Desktop/BAMBI/P10_group3_(200bp,80pc)_group(70pc).pdf", width=3.6, height=7.3);
-amrdata <- read.delim("/Users/leggettr/Desktop/BAMBI/P10_group3_(200bp,80pc)_group(70pc).txt", sep="\t",head=TRUE, check.names=FALSE)
+#pdf("/Users/leggettr/Desktop/BAMBI/P10_group3_(200bp,80pc)_group(70pc).pdf", width=3.6, height=6.6);
+#amrdata <- read.delim("/Users/leggettr/Desktop/BAMBI/P10_group3_(200bp,80pc)_group(70pc).txt", sep="\t",head=TRUE, check.names=FALSE)
 
 # #377eb8
 #pdf("/Users/leggettr/Desktop/BAMBI/P10_group1_(200bp,80pc)_group(70pc).pdf", width=4.5, height=2.3);
@@ -61,8 +65,8 @@ amrdata.m <- melt(amrdata)
 #amrdata.m <- ddply(amrdata.m, .(variable), transform, rescale=rescale(value))
 p <- ggplot(amrdata.m, aes(variable, Gene)) +
      geom_tile(aes(fill = value), colour = "white") +
-  #scale_fill_gradient(name = "Count", low = "#deebf7", high = "#3182bd", na.value = "#dddddd") +
-  scale_fill_gradient(name = "Count", low = "#3182bd", high = "#3182bd", na.value = "#dddddd") +
+     #scale_fill_gradient(name = "Count", low = "#deebf7", high = "#3182bd", na.value = "#dddddd") +
+     scale_fill_gradient(name = "Count", low = "#3182bd", high = "#3182bd", na.value = "#dddddd") +
      scale_x_discrete(position = "top") +
      scale_y_discrete(limits = rev(levels(amrdata.m$Gene))) +
      theme(legend.position="none", axis.ticks.x=element_blank(), axis.ticks.y=element_blank(), axis.title.x=element_blank(), axis.title.y=element_blank(), panel.background = element_blank(), axis.text.x = element_text(angle = 90, hjust = 0))
@@ -72,5 +76,6 @@ p <- ggplot(amrdata.m, aes(variable, Gene)) +
 #scale_fill_gradient(name = "Count", low = "#deebf7", high = "#3182bd", na.value = "white") +
   #theme(legend.position="right", axis.ticks.x=element_blank(), axis.ticks.y=element_blank(), axis.title.x=element_blank(), axis.title.y=element_blank(), panel.background = element_blank(), axis.text.x = element_text(angle = 90, hjust = 0))
 print(p)
+
 garbage <- dev.off()
 
